ef7ad1f8c99fae45b8d603ed62ff8af94ed5cf23
DaryaTereshchenko <daria.stetsenko97@gmail.com>
2024-09-18 22:57:52 +0200

    add prompt constructions, model testing

--- prompts_testing.py
+++ prompts_testing.py
@@ -1,19 +1,91 @@
 import openai
+import logging
+from tqdm import tqdm
+from utils import process_output, plot_count_and_normalized_confusion_matrix, log_metrics_and_confusion_matrices_wandb
+import pandas as pd
+import wandb
+seed = 42 # Seed for replicability of results
 
-client = openai.OpenAI(
-    base_url="http://localhost:11434/v1",
-    api_key="nokeyneeded",
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+WANDB_PROJECT_NAME = 'llm-classification'
 
-response = client.chat.completions.create(
-    model="phi3:medium",
-    temperature=0.7,
-    max_tokens=256,
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant who is fluent in Russian."},
-        {"role": "user", "content": "Напиши мне что-нибудь по-русски."},
-    ],
-)
 
-print("Response:")
-print(response.choices[0].message.content)
+def run_experiment(df: pd.DataFrame, system_prompt: str, user_prompt_format: str, 
+                   experiment_name:str, dataset_name: str, model_name: str,
+                   subset_output_fun: callable = lambda x: x):
+    # Start logging
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project=WANDB_PROJECT_NAME,
+        name=experiment_name,
+        # track hyperparameters and run metadata
+        config = {
+            "model": model_name,
+            "dataset": dataset_name
+        }
+    )
+    
+    # Log prompts
+    prompts_artifact = wandb.Artifact('prompts', type='prompts')
+    with prompts_artifact.new_file('system_prompt.txt', mode='w') as f:
+        f.write(system_prompt)
+    with prompts_artifact.new_file('user_prompt_format.txt', mode='w') as f:
+        f.write(user_prompt_format)
+    wandb.run.log_artifact(prompts_artifact)
+    
+    # Evaluate ChatGPT on the dataset
+    outputs = []
+    for text in tqdm(df.full_text):
+        # Get the evaluation from ChatGPT
+        chat_gpt_message_output = call_chat_completion_api(
+            client=client,
+            system_prompt=system_prompt, 
+            user_prompt=user_prompt_format.format(text=text),
+            force_json_output=False,
+            seed=seed,
+            model_name=model_name
+        )
+        
+        # Process the output
+        relevant, framing = process_output(subset_output_fun(chat_gpt_message_output))
+        
+        # Add to the list
+        outputs.append(
+            {
+                "relevant": relevant, 
+                "frame": framing,
+                "full_output_chatgpt": chat_gpt_message_output
+            }
+        )
+    
+    # Join the outputs to the original dataframe
+    outputs_df = pd.DataFrame(outputs, index=df.index)
+    df = df.join(outputs_df, rsuffix='_pred')
+    
+    # Log the dataset output to wandb
+    predictions_artifact = wandb.Artifact('predictions', type='outputs')
+    with predictions_artifact.new_file('predictions.csv', mode='w') as f:
+        df.to_csv(f)
+    wandb.run.log_artifact(predictions_artifact)
+    
+    # Measure performance 
+    try:
+        # Relevance task
+        y_true_relevance = df['relevant'].map(int)
+        y_pred_relevance = outputs_df['relevant'].fillna(-1).map(int)
+        cm_plot, classification_report, metrics  =  plot_count_and_normalized_confusion_matrix(y_true=y_true_relevance, y_pred=y_pred_relevance, labels=[1, 0], display_labels=['Relevant', 'Irrelevant'], custom_display_labels=True)
+        log_metrics_and_confusion_matrices_wandb(cm_plot=cm_plot, classification_report=classification_report, metrics=metrics, task_name='relevance')
+    
+        # Problem solution framing task
+        y_true_frame = df.loc[~df['frame'].isna(), 'frame'].map(
+            {'Neither': 0, 'Solution': 1, 'Problem': 2})
+        y_pred_frame = outputs_df.loc[~df['frame'].isna(), 'frame'].fillna(-1).map(int)
+    
+        cm_plot, classification_report, metrics = plot_count_and_normalized_confusion_matrix(y_true=y_true_frame, y_pred=y_pred_frame, labels=[0, 1, 2, 3], display_labels=['Neither', 'Solution', 'Problem', 'Both'], custom_display_labels=True)
+        log_metrics_and_confusion_matrices_wandb(cm_plot=cm_plot, classification_report=classification_report, metrics=metrics, task_name='framing')
+        wandb.finish()
+        
+    except Exception as e:
+        print('Error computing metrics: ', e)
+    
+    return df
---
+++ utils.py
@@ -0,0 +1,125 @@
+from typing import Any
+import regex as re 
+import numpy as np
+import json
+import wandb
+import logging
+import pandas as pd
+import pprint
+from sklearn.metrics import (accuracy_score, ConfusionMatrixDisplay, classification_report, recall_score,
+                             precision_score, f1_score)
+import matplotlib.pyplot as plt
+
+logging.basicConfig(level=logging.INFO, filename='inference.log', format='%(asctime)s - %(levelname)s - %(message)s')
+
+wandb.login()
+
+
+def extract_label_with_regex(input_string: str, pattern: str, group_to_return: int) -> float | Any:
+    # Use re.search to find the match
+    match = re.search(pattern, input_string)
+
+    if match:
+        # Extract the value of "relevance" from the match
+        value = str(match.group(group_to_return))
+        return value
+    else:
+        return np.nan
+    
+extract_label = lambda x: extract_label_with_regex(x, pattern=r'"label"\s*:\s*w+', group_to_return=2)
+
+
+
+def process_output(llm_message_output: str) -> list | Any:
+    try:
+        output = json.loads(llm_message_output)
+        get_label = 'label' if 'label' in output else None
+        
+        output_classes = output[get_label] if get_label else np.nan
+
+    except json.JSONDecodeError as e:
+        print(f'Error parsing JSON: {e}')
+        print(f'LLM Output: {llm_message_output}')
+        output_classes = extract_label(llm_message_output)
+    
+    except KeyError as e:
+        print(f'JSON does not contain the "label" in keys: {e}')
+        print(f'JSON: {output}')
+        output_classes = np.nan
+        
+    except ValueError as e:
+        print(f'Output classes could not be parsed as strings: {e}')
+        print(f'label: {output["label"]}')
+        
+        output_classes = output['label']
+    
+    except Exception as e:
+        print(f'Unknown error: {e}')
+        print(f'ChatGPT output: {llm_message_output}')
+        output_classes = np.nan
+
+    return output_classes
+
+def log_metrics_and_confusion_matrices_wandb(cm_plot, classification_report, metrics, task_name: str):
+    # Log metrics
+    for metric_name, metric_value in metrics.items():
+        wandb.log({f'{metric_name}_{task_name}': metric_value})
+
+    # Log the confusion matrix matplotlib figure
+    wandb.log({f'confusion_matrix [{task_name}]': wandb.Image(cm_plot)})
+
+    # Log the classification report as an artifact
+    classification_report = (pd.DataFrame({k: v for k, v in classification_report.items() if k != 'accuracy'})
+                             .transpose().reset_index())
+    
+    wandb.log({f'classification_report [{task_name}]': wandb.Table(
+        dataframe=classification_report)})
+    
+    classification_report_artifact = wandb.Artifact(
+        f'classification_report_{task_name}', type='classification_report')
+    
+    with classification_report_artifact.new_file(f'classification_report_{task_name}.txt', mode='w') as f:
+        f.write(pprint.pformat(classification_report))
+    wandb.run.log_artifact(classification_report_artifact)
+
+
+default_metrics = {
+    'accuracy': accuracy_score,
+    'recall': lambda y_t, y_p: recall_score(y_t, y_p, zero_division=np.nan, average='micro'),
+    'precision': lambda y_t, y_p: precision_score(y_t, y_p, zero_division=np.nan, average='micro'),
+    'f1': lambda y_t, y_p: f1_score(y_t, y_p, zero_division=np.nan, average='micro')
+}
+
+def plot_count_and_normalized_confusion_matrix(y_true, y_pred, display_labels, labels, xticks_rotation='horizontal',
+                                               metrics: dict = default_metrics, custom_display_labels: bool = False):
+    # Print classification report
+    cls_report = classification_report(y_true, y_pred, output_dict=True)
+    pprint.pprint(cls_report)
+
+    # Create plot with two subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+
+    # Remove labels and display_labels not present in y_true
+    if not custom_display_labels:
+        display_labels = [label for label in display_labels if label in y_true.unique()]
+        labels = [label for label in labels if label in y_true.unique()]
+
+    # Plot count confusion matrix
+    cm_disp = ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=labels, display_labels=display_labels)
+    cm_disp.plot(ax=ax1, xticks_rotation=xticks_rotation)
+    ax1.set_title('Count Confusion Matrix')
+
+    # Plot normalized confusion matrix
+    cm_disp = ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=labels, display_labels=display_labels,
+                                                      normalize='true')
+    cm_disp.plot(ax=ax2, xticks_rotation=xticks_rotation)
+    ax2.set_title('Normalized Confusion Matrix')
+
+    # Show plot
+    plt.show()
+    plt.close()
+
+    # Calculate metrics
+    metrics = {metric_name: metric_func(y_true, y_pred) for metric_name, metric_func in metrics.items()}
+
+    return fig, cls_report, metrics
