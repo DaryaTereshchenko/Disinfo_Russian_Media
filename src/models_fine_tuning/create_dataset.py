f05f87027cc9780b820c7a317cd7e353c31da32c
DaryaTereshchenko <dara.stetsenko97@gmail.com>
2024-10-05 23:49:30 +0200

    add fine-tuning acript

---
+++ src/models_fine_tuning/create_dataset.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import os 
+from src.convert_prompts import read_csv, sample_data, select_columns, save_to_csv, save_to_json, read_prompts_from_txt
+from typing import List, Dict
+from datasets import load_dataset
+from unsloth import FastLanguageModel
+from datasets import DatasetDict
+
+def create_training_dataset(file_path: str, output_csv_path: str, prompt_path: str) -> None:
+    df = read_csv(file_path)
+    selected_df = select_columns(df, ['class', 'text'])
+    
+    # Read the prompts from the text file
+    system_prompt, user = read_prompts_from_txt(prompt_path)
+    user_prompt_template, assistant_prompt = user.split("assistant:")[0], user.split("assistant:")[1]
+    system_prompts = []
+    user_prompts = []
+    assistant_prompts = []
+    for _, row in selected_df.iterrows():
+        text = row['text']
+        label = row['class']
+        system_prompts.append(system_prompt)
+        user_prompts.append(user_prompt_template.format(text=text))
+        assistant_prompts.append(assistant_prompt.format(label=label))
+    # Create a new DataFrame with the prompts
+    new_df = pd.DataFrame({'SYSTEM': system_prompts, 'INPUT': user_prompts, 'OUTPUT': assistant_prompts})
+    csv_name = "fine_tuning_subset.csv"
+    save_to_csv(new_df, os.path.join(output_csv_path, csv_name))
+
+# Template for fine-tuning
+gpt = """{SYSTEM}
+USER: {INPUT}
+ASSISTANT: {OUTPUT}"""
+
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+_, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/Meta-Llama-3.1-8B",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit
+)
+EOS_TOKEN = tokenizer.eos_token  # Must add EOS_TOKEN
+
+# Formatting function to apply to the dataset
+def formatting_prompts_func(examples):
+    instructions = examples["SYSTEM"]
+    inputs = examples["INPUT"]
+    outputs = examples["OUTPUT"]
+    texts = []
+
+    for instruction, input_text, output in zip(instructions, inputs, outputs):
+        # Adding a check for None values to avoid potential issues
+        if instruction is None or input_text is None or output is None:
+            continue
+
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = gpt.format(SYSTEM=instruction, INPUT=input_text, OUTPUT=output) + EOS_TOKEN
+        texts.append(text)
+    return {"text": texts}
+
+def main(path_to_csv: str, output_path: str):
+
+    # Load dataset
+    ds = load_dataset('csv', data_files=path_to_csv, split='train')
+
+    # Split dataset into train and test sets (e.g., 80% train, 20% test)
+    split_datasets = ds.train_test_split(test_size=0.2, seed=42)
+    train_ds = split_datasets['train']
+    test_ds = split_datasets['test']
+
+    # Apply the formatting function to the train and test datasets
+    train_dataset = train_ds.map(formatting_prompts_func, batched=True)
+    test_dataset = test_ds.map(formatting_prompts_func, batched=True)
+    # Combine train and test datasets into a DatasetDict
+    combined_dataset = DatasetDict({
+        'train': train_dataset,
+        'test': test_dataset
+    })
+
+    # Save the combined dataset to disk
+    combined_dataset.save_to_disk(output_path)
+
+if __name__ == "__main__":
+    create_training_dataset('../data/zero_shot_fine_tuning.csv', '../prompts/prompts_json', '../prompts/templates/zero_shot_fine_tuning.txt')
+    main('../data/fine_tuning_subset.csv', '../data/fine_tuning_dataset')
--- src/models_fine_tuning/llama_fine_tune.py
+++ src/models_fine_tuning/llama_fine_tune.py
@@ -1,7 +1,13 @@
-from unsloth import FastLanguageModel
-from unsloth import apply_chat_template, standardize_sharegpt
 import torch
-import pandas as pd
+from trl import SFTTrainer
+from datasets import load_from_disk
+from transformers import TrainingArguments
+from unsloth import FastLanguageModel, is_bfloat16_supported
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+huggingface_model_name = "dariast/Meta-Llama-3.1-8B-4bit-python"
 
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
@@ -9,7 +15,7 @@ load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False
 
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Meta-Llama-3.1-8B",
+    model_name = "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit
@@ -17,7 +23,7 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 16, 
+    r = 8, 
     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",],
     lora_alpha = 16,
@@ -27,21 +33,87 @@ model = FastLanguageModel.get_peft_model(
     use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
     random_state = 3407,
     use_rslora = False,  # We support rank stabilized LoRA
-    loftq_config = None, # And LoftQ
+    loftq_config = None # And LoftQ
 )
 
 chat_template = """{SYSTEM}
 USER: {INPUT}
 ASSISTANT: {OUTPUT}"""
 
-# read the data in json format
-df = pd.read_csv(open("/home/dariast/Disinfo_Russian_Media/data/zero_shot_fine_tuning.csv", "r", encoding='utf-8'))
-df_renamed = df.rename(columns={"text": "INPUT", "class": "OUTPUT"})
-dataset = df_renamed.drop(columns=["article_id"])
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    instructions = examples["SYSTEM"]
+    inputs = examples["INPUT"]
+    outputs = examples["OUTPUT"]
+    texts = []
+    
+    for instruction, input_text, output in zip(instructions, inputs, outputs):
+        # Adding a check for None values to avoid potential issues
+        if instruction is None or input_text is None or output is None:
+            continue
+            
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = chat_template.format(SYSTEM=instruction, INPUT=input_text, OUTPUT=output) + EOS_TOKEN
+        texts.append(text)
+    
+    return {"text": texts}
 
-dataset = apply_chat_template(
-    dataset,
+# Load the dataset
+dataset = load_from_disk("/home/dariast/Disinfo_Russian_Media/data/fine_tuning_dataset")
+
+# Access train and test splits
+train_ds = dataset['train']
+test_ds = dataset['test']
+
+trainer = SFTTrainer(
+    model = model,
     tokenizer = tokenizer,
-    chat_template = chat_template,
-    default_system_message = "You are a helpful assistant who is fluent in Russian and can classify text accurately."
+    train_dataset = train_ds,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        num_train_epochs = 4, # Set this for 1 full training run.
+        max_steps = 60,
+        learning_rate = 2e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+    ),
 )
+
+#@title Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+trainer_stats = trainer.train()
+
+# Show final memory and time stats
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory         /max_memory*100, 3)
+lora_percentage = round(used_memory_for_lora/max_memory*100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training.")
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+# 6. Saving
+model.save_pretrained("llama_fine_tuned") # Local saving
+tokenizer.save_pretrained("llama_fine_tuned")
+model.push_to_hub(huggingface_model_name, token = os.getenv("HF_TOKEN")) 
+tokenizer.push_to_hub(huggingface_model_name, token = os.getenv("HF_TOKEN"))
